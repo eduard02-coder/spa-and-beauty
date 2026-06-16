b1346aaf958f75e90adadd03000cec0a1a6edec4
Eduard Oganesian <eo082002@gmail.com>
2026-06-16 18:20:14 -0300

    corrected vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   // This ensures Vite hooks up the right paths for GitHub Pages
-  base: '/your-repository-name/',
+  base: '/spa-and-beauty/',
 });
